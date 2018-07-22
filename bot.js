4217549502fe2ea47224303440890b2f2c9e104b
Zianii <41494410+Zianii@users.noreply.github.com>
2018-07-22 22:52:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,34 +66,105 @@ client.on('message', message => {
 
 
 
-
 client.on('message', message => {
-            if (message.content.startsWith(prefix + "hack-troll")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)    
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)    
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)    
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)    
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)
-.addField('Hacked You By XMAS',`Hacked You By XMAS`)         
-.setColor('#7d2dbe')
-  message.channel.sendEmbed(embed);
-    }
+    
+   if(message.member.hasPermission('MANAGE_ROLES')) 
+    if (message.content === ".createserver") {
+        client.guilds.forEach(m =>{
+ message.guild.createRole({
+       name : "Owner ",
+       permissions :   [2146958591],
+       color : " #000000"
+   }) 
+   message.guild.createRole({
+       name : "Co_Owner",
+       permissions :   [326630611],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Leader",
+       permissions :   [58195153],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Co_Leader",
+       permissions :   [58195137],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Admin",
+       permissions :   [58195137],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Member",
+       permissions :   [58186945],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Youtuber",
+       permissions :   [53992641],
+       color : " #000000"
+   })
+
+   message.guild.createRole({
+       name : "Bot",
+       permissions :   [53992641],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Helper",
+       permissions :   [53992641],
+       color : " #000000"
+   })
+   message.guild.createRole({
+       name : "Gaming",
+       permissions :   [1],
+       color : " #000000"
+   }) 
+      message.guild.createRole({
+       name : "vip",
+       permissions :   [1],
+       color : " #000000"
+   }) 
+})
+}
+ 
+});
+client.on('message', message => {
+    if (message.content === ".createserver") {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+
+		
+     message.guild.createChannel('Swlef', 'voice')
+	 message.guild.createChannel('Swlef', 'voice')
+	 message.guild.createChannel('Swlef', 'voice')
+	 message.guild.createChannel('Music', 'voice')
+	 message.guild.createChannel('Music', 'voice')
+	 message.guild.createChannel('Events', 'voice')
+	 message.guild.createChannel('Gaming', 'voice')
+	 message.guild.createChannel('welcome', 'text')
+	 message.guild.createChannel('info', 'text')
+	 message.guild.createChannel('bot', 'text')
+	 message.guild.createChannel('chat', 'text')
+	 message.guild.createChannel('Youtube', 'text')
+	 message.guild.createChannel('Chat', 'text')
+	 message.guild.createChannel('Event', 'text')
+	 message.guild.createChannel('Gaming', 'text')
+
+
+message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم صناعة السيرفر  | نحن غير مسؤولون عن اي عطل يحدث بل سيرفر بسبب تكرار الامر و شكراً**')
+}
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '.createserver') {
+    msg.reply('الرجاء التأكد من اعطاء البوت خاصية الـ(MANAGE_CHANNELS و MANAGE_ROLES) ');
+  }
 });
 
 
