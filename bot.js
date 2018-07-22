59a5e90d059817268718ce54e503284f65898a0c
Zianii <41494410+Zianii@users.noreply.github.com>
2018-07-22 07:42:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`New Update :)`,"http://twitch.tv/xmas")
+client.user.setGame(`BETA !!`,"http://twitch.tv/xmas)
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
