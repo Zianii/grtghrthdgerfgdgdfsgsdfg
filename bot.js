a5538026d818e6b0ad09d45e9d1b194047227989
Zianii <41494410+Zianii@users.noreply.github.com>
2018-07-22 07:26:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -280,7 +280,7 @@ client.on('message' , async (message) => {
       color: 0x06DF00,
       description: "تم مسح الرسائل بنجاح",
       footer: {
-        text: "Name Bot."
+        text: "Void Bot !"
       }
     }}).then(msg => {msg.delete(3000)});
                         }
@@ -314,6 +314,54 @@ client.on('message', msg => {
       }
     }
 
+});
+
+
+
+```client.on('message', msg => {//msg
+    if (msg.content === 'الوان') {
+      if (msg.channel.id !== "463627632953786369") return;
+      msg.channel.send({file : "https://d.top4top.net/p_925trmdy1.png"})
+    }
+  });;
+  
+const Client = require('discord.js');
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+if(message.content.split(' ')[0] == 'لون'){
+if (message.channel.id !== "463627632953786369") return;
+     const embedd = new Discord.RichEmbed()
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setDescription(`**There's No Color With This Number ** ❌ `)
+.setColor(`ff0000`)
+
+if(!isNaN(args) && args.length > 0)
+
+
+ var a = message.guild.roles.find("name",`${args}`)
+          if(!a)return;
+          if (a.name > 250 || a.name < 1) return;
+const embed = new Discord.RichEmbed()
+              
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setDescription(`**Color Changed Successfully** ✅ `)
+
+.setColor(`${a.hexColor}`)
+message.channel.sendEmbed(embed);
+    if (!args)return;
+setInterval(function(){})
+            let count = 0;
+            let ecount = 0;
+  for(let x = 1; x < 201; x++){
+     
+      message.member.removeRole(message.guild.roles.find("name",`${x}`))
+    
+      }
+          message.member.addRole(message.guild.roles.find("name",`${args}`));
+  
+      
+}
 });
 
 
