0258bc79405e325257802726d74d56a4a525fc9e
Zianii <41494410+Zianii@users.noreply.github.com>
2018-07-22 07:47:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BETA !!`,"http://twitch.tv/xmas)
+  client.user.setGame(`BETA !!`,"http://twitch.tv/xmas")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,6 +27,300 @@ client.user.setGame(`BETA !!`,"http://twitch.tv/xmas)
   console.log('')
   console.log('')
 });
+
+
+
+
+
+
+client.on('message', message => {
+            if (message.content.startsWith(prefix + "bot")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
+.addField(' الاعضاء👥 ',` [${client.users.size}] `)
+.addField('الرومات📚 ',`[${client.channels.size}]`) 
+.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
+.addField('مصمم وصاحب البوت ',`XMAS`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+client.on('message', message => {
+            if (message.content.startsWith(prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' الاوامر الاداريه',` .help-admins `)
+.addField(' الاوامر العامه ',` .help-public `)
+.addField('اوامر الالوان',`قريبا`) 
+.addField(' اوامر الاغاني',`قريبا`) 
+.addField('مصمم وصاحب البوت ',`XMAS`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content === '.roles') {
+        var roles = message.guild.roles.map(roles => `${roles.name}, `).join(' ')
+        const embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .addField('الرتب:',`**[${roles}]**`)
+        message.channel.sendEmbed(embed);
+    }
+});
+
+ client.on('guildCreate', guild => {
+  client.channels.get("469981679746809857").send(`**تم اضافة البوت في سيرفر جديد مبروكك
+اسم السيرفر: __${guild.name}__
+اونر السيرفر: __${guild.owner}__**`)
+}); 
+
+
+             client.on('message', message => {
+                if(message.content === prefix + "invite") {
+                    let embed = new Discord.RichEmbed ()
+                    embed.setTitle("**:arrow_right: Invite Bot!**")
+                    .setURL("https://discordapp.com/api/oauth2/authorize?client_id=468528676775133184&permissions=2146958583&scope=bot");
+                   message.channel.sendEmbed(embed);
+                  }
+});
+
+
+             client.on('message', message => {
+                if(message.content === prefix + "support") {
+                    let embed = new Discord.RichEmbed ()
+                    embed.setTitle("**:arrow_right: Support Server Bot!**")
+                    .setURL("https://discord.gg/WBug9eX");
+                   message.channel.sendEmbed(embed);
+                  }
+});
+
+
+
+
+client.on('message', message => {
+     if (message.author.bot) return;
+       if (message.content ===  ".help-public") {
+ message.channel.send('**تم ارسال الاومر في الخاص|| Done Enter Command In Box :D**')
+message.author.sendMessage(`
+༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
+:earth_africa: الاوامــر الــعـــامـــة:earth_africa: 
+1༺༻  +id  | لعرض معلومات الشخص༺༻
+2༺༻  +info | لعرض معلومات عن البوت༺༻
+3༺༻  +server | لعرض معلومات السيرفر༺༻
+4༺༻  +ping | لعرض بنق البوت༺༻
+5༺༻  +date | لعرض التاريخ༺༻
+6༺༻ +invite | لدعوة البوت الى سيرفرك༺༻
+7༺༻  +invites | لعرض عدد الانفايت التي تمتلكه༺༻
+8༺༻  +avatar | لعرض صورة الشخص༺༻
+༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
+   `);
+       }
+});
+
+
+
+client.on('message', message => {
+     if (message.author.bot) return;
+       if (message.content ===  ".help-admins") {
+ message.channel.send('**تم ارسال الاومر في الخاص|| Done Enter Command In Box :D**')
+message.author.sendMessage(`
+༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
+:closed_lock_with_key: اوامـــر الادمـــنـــيــــة:closed_lock_with_key: 
+1༺༻  +clear | لمسح الشات༺༻
+2༺༻  +bc | لارسال رسالة لجميع الاعضاء༺༻
+3༺༻  +kick | لطرد العضو من السيرفر༺༻
+4༺༻  +ban | لاعطاء باند لشخص༺༻
+༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
+   `);
+       }
+});
+
+
+
+
+client.on('message', function(msg) {
+    const prefix = '.'
+    if(msg.content.startsWith (prefix  + 'server')) {
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+      .addField(':globe_with_meridians:** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+      .addField(':medal:** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+      .addField(':red_circle:**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+      .addField(':large_blue_circle:**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+      .addField(':pencil:**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+      .addField(':microphone:**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+      .addField(':crown:**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+      .addField(':id:**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+      .addField(':date:**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+      msg.channel.send({embed:embed});
+    }
+  });
+
+
+  
+  
+  client.on('message', message => {
+    const prefix = '.'
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'id')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
+
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
+
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing... |💤.";
+}
+let embed = new Discord.RichEmbed()
+.setColor("#502faf")
+.addField(': 🔱 | اسمك',`**<@` + `${z.id}` + `>**`, true)
+.addField(': 🛡 | ايديك', "**"+ `${z.id}` +"**",true)
+.addField(': ♨ | Playing','**'+y+'**' , true)
+.addField(': 📛 | تاق حق حسابك',"**#" +  `${z.discriminator}**`,true)
+.addField('**: 📆 | التاريح الذي انشئ فيه حسابك**', message.author.createdAt.toLocaleString())
+.addField("**: ⌚ | تاريخ دخولك للسيرفر**", message.member.joinedAt.toLocaleString())    
+
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+    if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
+
+}
+
+});
+
+
+
+
+
+
+
+    client.on('guildMemberRemove', member => {
+        var embed = new Discord.RichEmbed()
+        .setAuthor(member.user.username, member.user.avatarURL)
+        .setThumbnail(member.user.avatarURL)
+        .setTitle(`بس بعرف وين رحت؟؟؟ :raised_hand::skin-tone-1: :pensive:`)
+        .setDescription(`مع السلامه تشرفنا بك :raised_hand::skin-tone-1: :pensive: `)
+        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+        .setColor('RED')
+        .setFooter(`====ولكم منور السيرفر اتمنا لك الاستمتاع====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+    
+    var channel =member.guild.channels.find('name', '☞★٭『الترحيب』٭★☜')
+    if (!channel) return;
+    channel.send({embed : embed});
+    })
+	
+	
+	
+	
+	    client.on('message', msg => { 
+      if (msg.content.startsWith(`.sug`)) {
+         let args = msg.content.split(" ").slice(1);
+        if (!args[1]) return msg.reply(`يجب كتابه الاقتراح`)
+        if (msg.guild.channels.find('name', 'suggest')) {
+          msg.guild.channels.find('name', '☞★٭『اقتراحات』٭★☜').send(`
+        الاقتراح من : ${msg.member}
+        الاقتراح : **${args.join(" ").split(msg.mentions.members.first()).slice(' ')}**
+        `)
+        }
+      }
+      })
+	
+	
+  
+client.on('message' , async (message) => {
+ if (message.content.startsWith(prefix + 'pos')) {
+    // Lets define our array of guilds
+    const guildArray = client.guilds.map((guild) => {
+    return `${guild.name} : ${guild.id}`
+    })
+  
+    // And send it
+    message.channel.send(`\`\`\`${guildArray.join("\n")}\`\`\``)
+  }
+});
+  
+  
+  
+  
+  client.on("message", message => {
+  var prefix = ".";
+
+          var args = message.content.substring(prefix.length).split(" ");
+          if (message.content.startsWith(prefix + "clear")) {
+ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+      var msg;
+      msg = parseInt();
+    
+    message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+    message.channel.sendMessage("", {embed: {
+      title: "Done | تــم",
+      color: 0x06DF00,
+      description: "تم مسح الرسائل بنجاح",
+      footer: {
+        text: "Void Bot !"
+      }
+    }}).then(msg => {msg.delete(3000)});
+                        }
+
+   
+});
+
+
+
+
+client.on('message', msg => {
+  //Code By : ‡ ♪ ℬℐℓѦℓ✋ ‡#2026
+  if(msg.content.startsWith('.sug')) {
+    if(!msg.channel.guild) return msg.reply('** هاذا الامر فقط للسيرفرات**');
+    if(!msg.guild.channels.find('name', '☞★٭『اقتراحات』٭★☜')) return msg.reply('**الرجاء إضافة روم بإسم (☞★٭『اقتراحات』٭★☜)**');
+    let args = msg.content.split(" ").slice(1);
+    if(!args[1]) return msg.reply('الرجاء كتابة الاقتراح')
+    //غيره على حسب اسم روم الاقتراحات او سوي مثل اسم الروم الموجود هنا
+    if(msg.guild.channels.find('name', '☞★٭『اقتراحات』٭★☜')) {
+      //غيره هنا كمان اذا غيرت فوق
+      msg.guild.channels.find('name', '☞★٭『اقتراحات』٭★☜').send(`
+      تم الاقتراح من قبل : ${msg.member}
+
+      الاقتراح : 
+      ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
+      `)
+      .then(function (message) {
+        message.react('✅')
+        message.react('❌')
+      })
+      }
+    }
+
+});
+
+
+
+
 
 
 
