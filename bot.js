cd76484e32ad5afc17c76a79fffb8a78b31e766e
Zianii <41494410+Zianii@users.noreply.github.com>
2018-07-22 19:50:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`SUB TO KBOOSH`,"http://twitch.tv/xmas")
+  client.user.setGame(`تم استرجاع البوت | جددنا نظام الحمايه`,"http://twitch.tv/xmas")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
